aa645bf7a5d77bd2d60cd54ebd99179f5214dc2c
main2022 <41655930+team3star@users.noreply.github.com>
2022-08-15 08:11:25 +0900

    test ok

--- autoPictureDL_n_Like/convertURLtoArray.ps1
+++ autoPictureDL_n_Like/convertURLtoArray.ps1
@@ -5,10 +5,14 @@ if(!$result){
 }
 
 $logfile = './sampleLogs/twitter_array' + (Get-Date -Format "yyyyMMdd-HHmmss") + '.txt'
+$err_logfile = './sampleLogs/twitter_array' + (Get-Date -Format "yyyyMMdd-HHmmss") + '_err.txt'
+
 $TmpFile = New-TemporaryFile
 
 $array_from_txt = ( Get-Content -Encoding UTF8 ./image_URL_list.txt ).Replace("https://twitter.com/", "")
 $array_from_txt = $array_from_txt.Replace("https://mobile.twitter.com/", "")
+
+
 
 '
 Twitter Pictures URL SeleniumIDE(start)
@@ -17,7 +21,15 @@ Twitter Pictures URL SeleniumIDE(start)
 'return [' | Out-File $logfile -NoNewline -Append
 
 foreach($id in $array_from_txt) {
-    '"' + $id +'",' | Out-File $logfile -NoNewline -Append
+
+    if($id.IndexOf("status") -ne -1) {
+
+      '"' + $id +'",' | Out-File $logfile -NoNewline -Append
+
+    } else {
+      $id | Out-File $err_logfile -Append
+    
+    }
 }
 
 '""]' | Out-File $logfile -NoNewline -Append
